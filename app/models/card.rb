e8f3a1a8d5b51429062efa362469ec4195578fd6
sinjinrui <sinjin_ru@yahoo.co.jp>
2026-07-03 16:56:23 +0900

    ヤスミン追加

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,7 @@ class Card < ApplicationRecord
     001 002 003 004 005 006 007 008 009 010
     011 012 013 014 015 016 017 018 019 020
     021 022 023 024 025 026 027 028 029 030
+    031
     000
   ].freeze
 
@@ -39,7 +40,8 @@ class Card < ApplicationRecord
     "027" => "SAGAT",
     "028" => "C.VIPER",
     "029" => "ALEX",
-    "030" => "INGRID"
+    "030" => "INGRID",
+    "031" => "YASMINE"
   }.freeze
 
   belongs_to :user
